c13b95dcaab3339d5df58fd07962d41a558f4c45
unknown <mabg0630@gmail.com>
2025-03-02 14:49:09 -0500

    sll

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,4 +1,4 @@
 class GlobalConfig {
   // static const String apiHost = 'http://10.0.2.2:3000/api';
-  static const String apiHost = 'http://fitness.joperico.xyz/api';
+  static const String apiHost = 'https://fitness.joperico.xyz/api';
 }
